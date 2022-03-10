8d428ef02e1e72644868307b8865ea8170f3e17a
naiden <na4ko359@gmail.com>
2022-03-10 14:33:05 +0200

    update tip amount based on percentage

--- script/script.js
+++ script/script.js
@@ -1,26 +1,33 @@
 let tip = 15;
-// let bill = parseFloat(document.querySelector("#bill").value);
 let bill = document.querySelector("#bill");
-// let people = parseInt(document.querySelector("#people").value);
 let people = document.querySelector("#people");
 let total_per_person = document.querySelector("#total_per_person");
 let tip_per_person = document.querySelector("#tip_per_person");
 
-
 document.querySelector("#tip_5").addEventListener("click", () => {
     tip = 5;
+    total_per_person.textContent = ((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2);
+    tip_per_person.textContent = (((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2);
 });
 document.querySelector("#tip_10").addEventListener("click", () => {
     tip = 10;
+    total_per_person.textContent = ((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2);
+    tip_per_person.textContent = (((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2);
 });
 document.querySelector("#tip_15").addEventListener("click", () => {
     tip = 15;
+    total_per_person.textContent = ((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2);
+    tip_per_person.textContent = (((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2);
 });
 document.querySelector("#tip_25").addEventListener("click", () => {
     tip = 25;
+    total_per_person.textContent = ((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2);
+    tip_per_person.textContent = (((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2);
 });
 document.querySelector("#tip_50").addEventListener("click", () => {
     tip = 50;
+    total_per_person.textContent = ((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2);
+    tip_per_person.textContent = (((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2);
 });
 
 bill.addEventListener("input", () => {
