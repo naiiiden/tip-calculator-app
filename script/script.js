5e9dbd0ab093a61161571f07cf4ceacd86ae08b3
naiden <na4ko359@gmail.com>
2022-03-10 14:23:52 +0200

    tip values change dynamically

--- script/script.js
+++ script/script.js
@@ -1,8 +1,11 @@
 let tip = 15;
-let bill = parseFloat(document.querySelector("#bill").value);
+// let bill = parseFloat(document.querySelector("#bill").value);
+let bill = document.querySelector("#bill");
+// let people = parseInt(document.querySelector("#people").value);
+let people = document.querySelector("#people");
 let total_per_person = document.querySelector("#total_per_person");
 let tip_per_person = document.querySelector("#tip_per_person");
-let people = parseInt(document.querySelector("#people").value);
+
 
 document.querySelector("#tip_5").addEventListener("click", () => {
     tip = 5;
@@ -20,11 +23,19 @@ document.querySelector("#tip_50").addEventListener("click", () => {
     tip = 50;
 });
 
-console.log((((bill * tip) / 100) / people).toFixed(2));
 
-tip_per_person.textContent = (((bill * tip) / 100) / people).toFixed(2);
-total_per_person.textContent = ((bill + (bill * tip / 100)) / people).toFixed(2);
 
-// total_per_person.textContent = (parseFloat(bill + tip) / people).toFixed(2);
-// tip_per_person.textContent = ((total_per_person.textContent) / people).toFixed(2);
+
+
+
+bill.addEventListener("input", () => {
+    total_per_person.textContent = ((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2);
+    tip_per_person.textContent = (((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2);
+});
+people.addEventListener("input", () => {
+    total_per_person.textContent = ((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2);
+    tip_per_person.textContent = (((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2);
+});
+// tip_per_person.textContent = (((bill * tip) / 100) / people).toFixed(2);
+// total_per_person.textContent = ((bill + (bill * tip / 100)) / people).toFixed(2);
 
