d77746b3a9d3fc93f0737d31ee15222268cec9fb
naiden <na4ko359@gmail.com>
2022-03-11 14:35:34 +0200

    fixed missing $ sign on tip values

--- script/script.js
+++ script/script.js
@@ -5,8 +5,8 @@ let tip_per_person = document.querySelector("#tip_per_person");
 document.querySelectorAll(".input_container").forEach(input => {
     input.addEventListener("input", () => {
         document.querySelector("#reset").disabled = (this.value === "");
-        total_per_person.textContent = ((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2);
-        tip_per_person.textContent = (((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2);
+        total_per_person.textContent = `$${((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2)}`;
+        tip_per_person.textContent = `$${(((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2)}`;
     });
 });
 
@@ -19,34 +19,34 @@ document.querySelectorAll(".input_container").forEach(input => {
 });
 
 document.querySelector("#custom").addEventListener("input", () => {
-    total_per_person.textContent = ((parseFloat(bill.value) + ((parseFloat(bill.value) * document.querySelector("#custom").value / 100))) / people.value).toFixed(2);
-    tip_per_person.textContent = (((parseFloat(bill.value) * document.querySelector("#custom").value) / 100) / people.value).toFixed(2);
+    total_per_person.textContent = `$${((parseFloat(bill.value) + ((parseFloat(bill.value) * document.querySelector("#custom").value / 100))) / people.value).toFixed(2)}`;
+    tip_per_person.textContent = `$${(((parseFloat(bill.value) * document.querySelector("#custom").value) / 100) / people.value).toFixed(2)}`;
 });
 
 document.querySelector("#tip_5").addEventListener("click", () => {
     tip = 5;
-    total_per_person.textContent = ((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2);
-    tip_per_person.textContent = (((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2);
+    total_per_person.textContent = `$${((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2)}`;
+    tip_per_person.textContent = `$${(((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2)}`;
 });
 document.querySelector("#tip_10").addEventListener("click", () => {
     tip = 10;
-    total_per_person.textContent = ((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2);
-    tip_per_person.textContent = (((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2);
+    total_per_person.textContent = `$${((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2)}`;
+    tip_per_person.textContent = `$${(((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2)}`;
 });
 document.querySelector("#tip_15").addEventListener("click", () => {
     tip = 15;
-    total_per_person.textContent = ((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2);
-    tip_per_person.textContent = (((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2);
+    total_per_person.textContent = `$${((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2)}`;
+    tip_per_person.textContent = `$${(((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2)}`;
 });
 document.querySelector("#tip_25").addEventListener("click", () => {
     tip = 25;
-    total_per_person.textContent = ((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2);
-    tip_per_person.textContent = (((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2);
+    total_per_person.textContent = `$${((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2)}`;
+    tip_per_person.textContent = `$${(((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2)}`;
 });
 document.querySelector("#tip_50").addEventListener("click", () => {
     tip = 50;
-    total_per_person.textContent = ((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2);
-    tip_per_person.textContent = (((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2);
+    total_per_person.textContent = `$${((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2)}`;
+    tip_per_person.textContent = `$${(((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2)}`;
 });
 
 document.querySelector("#reset").addEventListener("click", () => {
