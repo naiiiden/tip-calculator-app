78143ed7b08485a6f4c0a8efc05c54dc4d634a0e
naiden <na4ko359@gmail.com>
2022-03-11 16:59:13 +0200

    js improvements

--- script/script.js
+++ script/script.js
@@ -5,8 +5,19 @@ let tip_per_person = document.querySelector("#tip_per_person");
 document.querySelectorAll(".input_container").forEach(input => {
     input.addEventListener("input", () => {
         document.querySelector("#reset").disabled = (this.value === "");
-        total_per_person.textContent = `$${((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2)}`;
-        tip_per_person.textContent = `$${(((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2)}`;
+        if (document.querySelector("#bill").value === "" && document.querySelector("#people").value === "") {
+            total_per_person.textContent = "$0.00";
+            tip_per_person.textContent = "$0.00";
+        } else if (document.querySelector("#people").value === "" || document.querySelector("#bill").value === "") {
+            document.querySelector("#people").classList.add("error_style");
+            document.querySelector("#people").placeholder = "Can't be zero";
+        } else {
+            document.querySelector("#people").style.borderColor = "transparent";
+            document.querySelector("#people").classList.remove("error_style");
+            document.querySelector("#people").placeholder = "0";
+            total_per_person.textContent = `$${((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2)}`;
+            tip_per_person.textContent = `$${(((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2)}`;
+        }
     });
     input.addEventListener("keypress", e => {
         if (!/[0-9\./]+/.test(e.key)) { //PREVENTS FROM TYPING CHARACTERS WHICH ARE DIFFERENT THAN 0-9 AND .
@@ -16,39 +27,71 @@ document.querySelectorAll(".input_container").forEach(input => {
 });
 
 document.querySelector("#custom").addEventListener("input", () => {
-    total_per_person.textContent = `$${((parseFloat(bill.value) + ((parseFloat(bill.value) * document.querySelector("#custom").value / 100))) / people.value).toFixed(2)}`;
-    tip_per_person.textContent = `$${(((parseFloat(bill.value) * document.querySelector("#custom").value) / 100) / people.value).toFixed(2)}`;
+    if (document.querySelector("#bill").value === "" && document.querySelector("#people").value === "") {
+        total_per_person.textContent = "$0.00";
+        tip_per_person.textContent = "$0.00";
+    } else {
+        total_per_person.textContent = `$${((parseFloat(bill.value) + ((parseFloat(bill.value) * document.querySelector("#custom").value / 100))) / people.value).toFixed(2)}`;
+        tip_per_person.textContent = `$${(((parseFloat(bill.value) * document.querySelector("#custom").value) / 100) / people.value).toFixed(2)}`;
+    }
 });
 
 document.querySelector("#tip_5").addEventListener("click", () => {
     tip = 5;
-    total_per_person.textContent = `$${((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2)}`;
-    tip_per_person.textContent = `$${(((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2)}`;
+    if (document.querySelector("#bill").value === "" && document.querySelector("#people").value === "") {
+        total_per_person.textContent = "$0.00";
+        tip_per_person.textContent = "$0.00";
+    } else {
+        total_per_person.textContent = `$${((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2)}`;
+        tip_per_person.textContent = `$${(((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2)}`;
+    }
 });
 document.querySelector("#tip_10").addEventListener("click", () => {
     tip = 10;
-    total_per_person.textContent = `$${((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2)}`;
-    tip_per_person.textContent = `$${(((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2)}`;
+    if (document.querySelector("#bill").value === "" && document.querySelector("#people").value === "") {
+        total_per_person.textContent = "$0.00";
+        tip_per_person.textContent = "$0.00";
+    } else {
+        total_per_person.textContent = `$${((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2)}`;
+        tip_per_person.textContent = `$${(((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2)}`;
+    }
 });
 document.querySelector("#tip_15").addEventListener("click", () => {
     tip = 15;
-    total_per_person.textContent = `$${((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2)}`;
-    tip_per_person.textContent = `$${(((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2)}`;
+    if (document.querySelector("#bill").value === "" && document.querySelector("#people").value === "") {
+        total_per_person.textContent = "$0.00";
+        tip_per_person.textContent = "$0.00";
+    } else {
+        total_per_person.textContent = `$${((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2)}`;
+        tip_per_person.textContent = `$${(((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2)}`;
+    }
 });
 document.querySelector("#tip_25").addEventListener("click", () => {
     tip = 25;
-    total_per_person.textContent = `$${((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2)}`;
-    tip_per_person.textContent = `$${(((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2)}`;
+    if (document.querySelector("#bill").value === "" && document.querySelector("#people").value === "") {
+        total_per_person.textContent = "$0.00";
+        tip_per_person.textContent = "$0.00";
+    } else {
+        total_per_person.textContent = `$${((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2)}`;
+        tip_per_person.textContent = `$${(((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2)}`;
+    }
 });
 document.querySelector("#tip_50").addEventListener("click", () => {
     tip = 50;
-    total_per_person.textContent = `$${((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2)}`;
-    tip_per_person.textContent = `$${(((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2)}`;
+    if (document.querySelector("#bill").value === "" && document.querySelector("#people").value === "") {
+        total_per_person.textContent = "$0.00";
+        tip_per_person.textContent = "$0.00";
+    } else {
+        total_per_person.textContent = `$${((parseFloat(bill.value) + ((parseFloat(bill.value) * tip / 100))) / people.value).toFixed(2)}`;
+        tip_per_person.textContent = `$${(((parseFloat(bill.value) * tip) / 100) / people.value).toFixed(2)}`;
+    }
 });
 
 document.querySelector("#reset").addEventListener("click", () => {
     total_per_person.textContent = "$0.00";
     tip_per_person.textContent = "$0.00";
+    document.querySelector("#people").placeholder = "0";
+    document.querySelector("#people").classList.remove("error_style");
     document.querySelectorAll(".input_container").forEach(input => {
         input.value = "";
     });
