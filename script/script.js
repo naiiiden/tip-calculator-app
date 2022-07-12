319e13e9e99988b46c1a7eab0af35a495d5e43a0
naiiiden <na4ko359@gmail.com>
2022-07-12 14:55:50 +0300

    input containers improvements

--- script/script.js
+++ script/script.js
@@ -8,18 +8,19 @@ document.querySelectorAll(".input_container").forEach(input => {
         if (document.querySelector("#bill").value === "" && document.querySelector("#people").value === "") {
             total_per_person.textContent = "$0.00";
             tip_per_person.textContent = "$0.00";
-        } 
-        else if (document.querySelector("#people").value === "" || document.querySelector("#bill").value === "") {
+        } else if (document.querySelector("#people").value === "" || document.querySelector("#bill").value === "") {
             document.querySelector("#people").classList.add("error_style");
             document.querySelector("#people").placeholder = "Can't be zero";
-        } 
-        else if (document.querySelector("#bill").value !== "" && document.querySelector("#people").value === "")  {
+            document.querySelector("#bill").classList.add("error_style");
+            document.querySelector("#bill").placeholder = "Can't be zero";
+        } else if (document.querySelector("#bill").value !== "" && document.querySelector("#people").value === "")  {
             document.querySelector("#people").style.borderColor = "#ff525d";
-        } 
-        else if (document.querySelector("#people").value === "") {
+        } else if (document.querySelector("#people").value === "") {
             document.querySelector("#people").style.borderColor = "#ff525d";
-        } 
-        else {
+        } else {
+            document.querySelector("#bill").style.borderColor = "transparent";
+            document.querySelector("#bill").classList.remove("error_style");
+            document.querySelector("#bill").placeholder = "0";
             document.querySelector("#people").style.borderColor = "transparent";
             document.querySelector("#people").classList.remove("error_style");
             document.querySelector("#people").placeholder = "0";
@@ -32,6 +33,33 @@ document.querySelectorAll(".input_container").forEach(input => {
             e.preventDefault();
         };
     });
+});
+
+document.querySelector("#bill").addEventListener("input", () => {
+    if (document.querySelector("#people").value === "") {
+        document.querySelector("#people").style.borderColor = "#ff525d";
+    };
+    if (document.querySelector("#bill").value === "") {
+        document.querySelector("#bill").style.borderColor = "#ff525d";
+    };
+    if (document.querySelector("#bill").value === "" && document.querySelector("#people").value === "") {
+        document.querySelector("#bill").style.borderColor = "#ff525d";
+        document.querySelector("#people").style.borderColor = "#ff525d";
+    };
+});
+
+document.querySelector("#people").addEventListener("input", () => {
+    if (document.querySelector("#people").value === "") {
+        document.querySelector("#people").style.borderColor = "#ff525d";
+    };
+    if (document.querySelector("#bill").value === "") {
+        document.querySelector("#bill").style.borderColor = "#ff525d";
+        document.querySelector("#bill").classList.add("error_style");
+        document.querySelector("#bill").placeholder = "Can't be zero";
+    };
+    if (document.querySelector("#people").value !== "") {
+        document.querySelector("#people").style.borderColor = "transparent";
+    };
 });
 
 document.querySelector("#people").addEventListener("click", () => {
@@ -115,10 +143,16 @@ document.querySelector("#tip_50").addEventListener("click", () => {
 });
 
 document.querySelector("#reset").addEventListener("click", () => {
+    tip = 15;
+    document.querySelector("#tip_15").checked = true;
     total_per_person.textContent = "$0.00";
     tip_per_person.textContent = "$0.00";
+    document.querySelector("#bill").placeholder = "0";
+    document.querySelector("#bill").classList.remove("error_style");
     document.querySelector("#people").placeholder = "0";
     document.querySelector("#people").classList.remove("error_style");
+    document.querySelector("#people").style.borderColor = "transparent";
+    document.querySelector("#bill").style.borderColor = "transparent";
     document.querySelector("#custom").style.borderColor = "transparent";
     document.querySelectorAll(".input_container").forEach(input => {
         input.value = "";
